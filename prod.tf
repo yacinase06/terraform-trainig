1c478c4319c27705938e38c930d799cc7d30971b
Yacine BENAMRI <yacine.benamri@gmail.com>
2021-07-10 23:31:55 +0200

    add security group to aws

--- prod.tf
+++ prod.tf
@@ -14,3 +14,31 @@ resource "aws_s3_bucket" "prod_tf_course" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_sg"{
+  name        = "prod-sg-web"
+  description = "allow std http/https port inbound + Everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+  "Terraform" : "true"
+  }
+
+}
